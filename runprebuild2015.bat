d8792ac3ab189a701f8f4154ce1fd0d7ab757b29
Jim Tarber <jim@gridmail.org>
2016-02-17 21:26:39 -0400

    Misc root folder cleanup
    - Fixed opensim.sln references to halcyon.sln
    - also disabled writing the compile.bat file for now since we only use
    the VS solutions
    - removed the "coming soon" from the readme

--- runprebuild2015.bat
+++ runprebuild2015.bat
@@ -1,3 +1,2 @@
 bin\Prebuild.exe /target vs2015
-echo C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild opensim.sln > compile.bat
-
+@rem echo C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild halcyon.sln > compile.bat
