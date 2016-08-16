74df27133970e4c5c6674c96bf7040c52dfd8e5c
David Daeschler <david.daeschler@gmail.com>
2016-08-16 17:32:15 -0500

    Working on JWT user auth and apply Resharper settings

---
+++ OpenSim/Framework/Communications/JWT/JWTUserAuthenticationGateway.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSim.Framework.Communications.JWT
+{
+    class JWTUserAuthenticationGateway
+    {
+        private IUserService m_userService;
+
+        public JWTUserAuthenticationGateway(IUserService userService)
+        {
+            m_userService = userService;
+
+        }
+    }
+}
---
+++ OpenSim/Framework/Communications/JWT/PayloadOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OpenSim.Framework.Communications.JWT
+{
+    /// <summary>
+    /// Options that can are passed through a JWT authentication payload
+    /// </summary>
+    class PayloadOptions
+    {
+        /// <summary>
+        /// The expiration date for this payload
+        /// </summary>
+        public DateTime Exp { get; set; }
+
+        /// <summary>
+        /// The auth scope that this authentication token applies to (eg: remote-console)
+        /// </summary>
+        public string Scope { get; set; }
+
+        /// <summary>
+        /// The username that was used to generate this authentication payload
+        /// </summary>
+        public string Username { get; set; }
+    }
+}
