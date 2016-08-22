IslandzVW/halcyon

commit b4031c2b83a776e748ed2e335d7022a9e33501b4

Added the ability to prepend the Data Source to the MySQL conection string for the RDB connection template.
This allows me to then remove that section from the rdb_connection_template stanza of the Halcyon.ini file, removing a potentially problematic spot for overzealous INI file editors - humans or automated.

For example, here's the origial stanza:

```
    rdb_connection_template = "Data Source={0};Database=DATABASE;User ID=USERNAME;password=PASSWORD;Pooling=True;Min Pool Size=0;"
```
Note the potentially confusing, especially to those who are note at all familiar with C# string formatting, "{0}" piece.

And the result after the upcoming commit:

```
    rdb_connection_template = "Database=DATABASE;User ID=USERNAME;password=PASSWORD;Pooling=True;Min Pool Size=0;"
```

OpenSim/Region/CoreModules/Avatar/Search/AvatarSearchModule.cs
@@ -116,7 +116,15 @@ namespace OpenSim.Region.CoreModules.Avatar.Search
             IConfig myConfig = config.Configs["Startup"];
             string connstr = myConfig.GetString("core_connection_string", String.Empty);
             _rdbConnectionTemplate = myConfig.GetString("rdb_connection_template", String.Empty);
+            if (!_rdbConnectionTemplate.Contains("Data Source"))
+            {
+                _rdbConnectionTemplate = "Data Source={0};" + _rdbConnectionTemplate;
+            }
             _rdbConnectionTemplateDebug = myConfig.GetString("rdb_connection_template_debug", String.Empty);
+            if (!_rdbConnectionTemplateDebug.Contains("Data Source"))
+            {
+                _rdbConnectionTemplateDebug = "Data Source={0};" + _rdbConnectionTemplateDebug;
+            }
             _connFactory = new ConnectionFactory("MySQL", connstr);
 
             CacheRdbHosts();

OpenSim/Region/CoreModules/World/Land/LandManagementModule.cs
@@ -117,7 +117,15 @@ namespace OpenSim.Region.CoreModules.World.Land
             IConfig myConfig = config.Configs["Startup"];
             string connstr = myConfig.GetString("core_connection_string", String.Empty);
             _rdbConnectionTemplate = myConfig.GetString("rdb_connection_template", String.Empty);
+            if (!_rdbConnectionTemplate.Contains("Data Source"))
+            {
+                _rdbConnectionTemplate = "Data Source={0};" + _rdbConnectionTemplate;
+            }
             _rdbConnectionTemplateDebug = myConfig.GetString("rdb_connection_template_debug", String.Empty);
+            if (!_rdbConnectionTemplateDebug.Contains("Data Source"))
+            {
+                _rdbConnectionTemplateDebug = "Data Source={0};" + _rdbConnectionTemplateDebug;
+            }
             _connFactory = new ConnectionFactory("MySQL", connstr);
 
             CacheRdbHosts();

commit 4b5e96134f0baf80788a55a11fdf2bae2332248d

Fixes the regression in the db connect string prepending enhancement
Fixes a problem introduced in
https://github.com/InWorldz/halcyon/commit/b4031c2b83a776e748ed2e335d7022a9e33501b4
where the _rdbConnectionTemplateDebug was used instead of
_rdbConnectionTemplate, and with only a data source (no credentials), if
missing from the INI file (like on IW production machines).  Also
removes case-sensitivity on the "Data Source" existence check.

OpenSim/Region/CoreModules/Avatar/Search/AvatarSearchModule.cs
@@ -116,14 +116,20 @@ namespace OpenSim.Region.CoreModules.Avatar.Search
             IConfig myConfig = config.Configs["Startup"];
             string connstr = myConfig.GetString("core_connection_string", String.Empty);
             _rdbConnectionTemplate = myConfig.GetString("rdb_connection_template", String.Empty);
-            if (!_rdbConnectionTemplate.Contains("Data Source"))
+            if (!String.IsNullOrWhiteSpace(_rdbConnectionTemplate))
             {
-                _rdbConnectionTemplate = "Data Source={0};" + _rdbConnectionTemplate;
+                if (!_rdbConnectionTemplate.ToLower().Contains("data source"))
+                {
+                    _rdbConnectionTemplate = "Data Source={0};" + _rdbConnectionTemplate;
+                }
             }
             _rdbConnectionTemplateDebug = myConfig.GetString("rdb_connection_template_debug", String.Empty);
-            if (!_rdbConnectionTemplateDebug.Contains("Data Source"))
+            if (!String.IsNullOrWhiteSpace(_rdbConnectionTemplateDebug))
             {
-                _rdbConnectionTemplateDebug = "Data Source={0};" + _rdbConnectionTemplateDebug;
+                if (!_rdbConnectionTemplateDebug.ToLower().Contains("data source"))
+                {
+                    _rdbConnectionTemplateDebug = "Data Source={0};" + _rdbConnectionTemplateDebug;
+                }
             }
             _connFactory = new ConnectionFactory("MySQL", connstr);
 

OpenSim/Region/CoreModules/World/Land/LandManagementModule.cs
@@ -117,14 +117,20 @@ namespace OpenSim.Region.CoreModules.World.Land
             IConfig myConfig = config.Configs["Startup"];
             string connstr = myConfig.GetString("core_connection_string", String.Empty);
             _rdbConnectionTemplate = myConfig.GetString("rdb_connection_template", String.Empty);
-            if (!_rdbConnectionTemplate.Contains("Data Source"))
+            if (!String.IsNullOrWhiteSpace(_rdbConnectionTemplate))
             {
-                _rdbConnectionTemplate = "Data Source={0};" + _rdbConnectionTemplate;
+                if (!_rdbConnectionTemplate.ToLower().Contains("data source"))
+                {
+                    _rdbConnectionTemplate = "Data Source={0};" + _rdbConnectionTemplate;
+                }
             }
             _rdbConnectionTemplateDebug = myConfig.GetString("rdb_connection_template_debug", String.Empty);
-            if (!_rdbConnectionTemplateDebug.Contains("Data Source"))
+            if (!String.IsNullOrWhiteSpace(_rdbConnectionTemplateDebug))
             {
-                _rdbConnectionTemplateDebug = "Data Source={0};" + _rdbConnectionTemplateDebug;
+                if (!_rdbConnectionTemplateDebug.ToLower().Contains("data source"))
+                {
+                    _rdbConnectionTemplateDebug = "Data Source={0};" + _rdbConnectionTemplateDebug;
+                }
             }
             _connFactory = new ConnectionFactory("MySQL", connstr);
 