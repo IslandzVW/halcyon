c4d38837683390e00fbcec1e2b52999f61a242b4
Paul Whittemore <dev@appurist.com>
2017-01-19 04:40:54 -0400

    Added rough (no-persist) support for AgentPreferences CAP
    Also lacking sending an update to other agents on a hover change.

---
+++ OpenSim/Framework/AgentPreferencesData.cs
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) InWorldz Halcyon Developers
+ * Copyright (c) Contributors, http://opensimulator.org/
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the OpenSim Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System.Collections.Generic;
+using OpenMetaverse;
+using OpenMetaverse.StructuredData;
+
+namespace OpenSim.Framework
+{
+    /// <summary>
+    /// Known information about a particular user preferences
+    /// </summary>
+    public class AgentPreferencesData
+    {
+        public UUID PrincipalID = UUID.Zero;
+        public string AccessPrefs = "M";
+        //public int GodLevel; // *TODO: Implement GodLevel (Unused by the viewer, afaict - 6/11/2015)
+        public double HoverHeight = 0.0;
+        public string Language = "en-us";
+        public bool LanguageIsPublic = true;
+        // DefaultObjectPermMasks
+        public int PermEveryone = 0;
+        public int PermGroup = 0;
+        public int PermNextOwner = 0; // Illegal value by design
+
+        public AgentPreferencesData()
+        {
+        }
+
+        public AgentPreferencesData(OSDMap map)
+        {
+            this.FromOSDMap(map);
+        }
+
+        public OSDMap ToOSDMap()
+        {
+            OSDMap result = new OSDMap();
+            result["PrincipalID"] = PrincipalID;
+            result["AccessPrefs"] = AccessPrefs;
+            result["HoverHeight"] = HoverHeight;
+            result["Language"] = Language;
+            result["LanguageIsPublic"] = LanguageIsPublic;
+            result["PermEveryone"] = PermEveryone;
+            result["PermGroup"] = PermGroup;
+            result["PermNextOwner"] = PermNextOwner;
+            return result;
+        }
+
+        public void FromOSDMap(OSDMap map)
+        {
+            if (map.ContainsKey("PrincipalID"))
+                UUID.TryParse(map["PrincipalID"].ToString(), out PrincipalID);
+            if (map.ContainsKey("AccessPrefs"))
+                AccessPrefs = map["AccessPrefs"].ToString();
+            if (map.ContainsKey("HoverHeight"))
+                HoverHeight = double.Parse(map["HoverHeight"].ToString());
+            if (map.ContainsKey("Language"))
+                Language = map["Language"].ToString();
+            if (map.ContainsKey("LanguageIsPublic"))
+                LanguageIsPublic = bool.Parse(map["LanguageIsPublic"].ToString());
+            if (map.ContainsKey("PermEveryone"))
+                PermEveryone = int.Parse(map["PermEveryone"].ToString());
+            if (map.ContainsKey("PermGroup"))
+                PermGroup = int.Parse(map["PermGroup"].ToString());
+            if (map.ContainsKey("PermNextOwner"))
+                PermNextOwner = int.Parse(map["PermNextOwner"].ToString());            
+        }
+    }
+}
