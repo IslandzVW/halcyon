af790a3ba447cf4b74da852455022a204a42f6c4
David Daeschler <david.daeschler@gmail.com>
2017-02-23 17:04:09 -0600

    Add Halcyon group

--- InWorldz/InWorldz.Arbiter/Definitions/generate.bat
+++ InWorldz/InWorldz.Arbiter/Definitions/generate.bat
@@ -2,6 +2,7 @@
 ..\..\..\bin\flatc-win.exe --csharp Quaternion.fbs
 ..\..\..\bin\flatc-win.exe --csharp HalcyonPrimitiveBaseShape.fbs
 ..\..\..\bin\flatc-win.exe --csharp HalcyonPrimitive.fbs
+..\..\..\bin\flatc-win.exe --csharp HalcyonGroup.fbs
 
 move /Y .\InWorldz\Arbiter\Serialization\* ..\Serialization
-rmdir /S /Y .\InWorldz
+rmdir /S/Q .\InWorldz
