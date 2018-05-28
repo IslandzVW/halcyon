d7a59c1f69cf8ea8e4fdb13963291fab94e95db6
Paul / Jim (Appurist) <dev@appurist.com>
2018-05-28 01:42:10 -0300

    We only have one special customType for deletes, removed the dictionary and cleaned up all that code.

--- OpenSim/Framework/Communications/UserProfileManager.cs
+++ OpenSim/Framework/Communications/UserProfileManager.cs
@@ -127,8 +127,6 @@ namespace OpenSim.Framework.Communications
         private readonly Dictionary<string, CachedUserInfo> m_userInfoByName
             = new Dictionary<string, CachedUserInfo>();
 
-        private Dictionary<String, UUID> customUserMap = new Dictionary<string, UUID>();
-
         public const string CUSTOM_TYPE_DELETED = "DELETED";
         private UUID m_deletedUserAccount = UUID.Zero;
 
@@ -153,7 +151,6 @@ namespace OpenSim.Framework.Communications
                 UUID uuid = UUID.Zero;
                 if (UUID.TryParse(deletedStr.Trim(), out uuid))
                 {
-                    customUserMap.Add(CUSTOM_TYPE_DELETED, uuid);
                     m_deletedUserAccount = uuid;
                 }
             }
@@ -301,17 +298,16 @@ namespace OpenSim.Framework.Communications
             if (profile == null)
                 return null;
 
-            // Check if it's a special
-            string customType = profile.CustomType.Trim().ToUpper();
-            UUID remappedID = UUID.Zero;
-            if (!customUserMap.TryGetValue(customType, out remappedID))
+            // Continue on right away if it's not a deleted account.
+            if (profile.CustomType.Trim().ToUpper() != CUSTOM_TYPE_DELETED)
                 return profile;
 
-            if (profile.ID == remappedID)
+            // Check if it's THE substitute deleted account
+            if (profile.ID == DeletedUserAccount)
                 return profile; // avoid infinite recursion
 
-            // Otherwise it's a special customType (DELETED) and remap the UUID to the special account.
-            return GetUserProfile(remappedID);
+            // Otherwise it's a special customType==DELETED and remap the UUID to the special account.
+            return GetUserProfile(DeletedUserAccount);
         }
 
         private UserProfileData _GetUserProfileData(UUID uuid)
